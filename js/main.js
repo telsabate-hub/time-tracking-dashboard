95c296095b8a3c3f404462dd76f391766e542784
Tel <telsabate.business@gmail.com>
2025-04-03 22:21:49 +0800

    Added fetch call to get data.json file. Added functions to populate UI based on data and added click event listener for the menu items.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,69 @@
+let timeframeData;
+let selectedTimeframe = "weekly";
+
+function getData(){
+    fetch('/data.json').then((response) => {  
+        if(!response.ok) return console.log('Oops! Something went wrong.');
+    
+        return response.json();
+    }).then((data) => {
+        timeframeData = data;
+
+        setTimeframes();
+    });
+}
+
+function setTimeframes(){
+    timeframeData.forEach( (category) => {
+        let title = category.title;
+        let container = document.getElementById( `${title.toLowerCase().replace( ' ', '' )}Container` );
+        let currentValueElem = container.querySelector( `.category-current-value` );
+        let previousValueElem = container.querySelector( `.category-previous-value` );
+
+        currentValueElem.dataset.current = category.timeframes[ selectedTimeframe ].current;
+        previousValueElem.dataset.previous = category.timeframes[ selectedTimeframe ].previous;
+    });
+
+    populateData();
+}
+
 function populateData(){
+    const prefixes = {
+        daily: "Yesterday",
+        weekly: "Last Week",
+        monthly: "Last Month"
+    };
+
     const allCurrentValues = document.querySelectorAll( ".category-current-value" );
     const allPreviousValues = document.querySelectorAll( ".category-previous-value" );
 
     allCurrentValues.forEach( (currentValue) => {
-        currentValue.innerHTML = `${currentValue.dataset.current}hrs`;
+        let currentValueData = currentValue.dataset.current;
+        let units = Number(currentValueData) == 1 ? "hr" : "hrs";
+
+        currentValue.innerHTML = `${currentValueData}${units}`;
     });
 
     allPreviousValues.forEach( (previousValue) => {
-        previousValue.innerHTML = `Last Week - ${previousValue.dataset.previous}hrs`;
+        let previousValueData = previousValue.dataset.previous;
+        let units = Number(previousValueData) == 1 ? "hr" : "hrs";
+
+        previousValue.innerHTML = `${prefixes[selectedTimeframe]} - ${previousValueData}${units}`;
     });
 }
 
-populateData();
+getData();
+
+function clickMenu(e){
+    const prevSelected = document.getElementsByClassName( "selected-timeframe" )[0];
+    let target = e.target;
+
+    prevSelected.classList.remove( "selected-timeframe" );
+    target.classList.add( "selected-timeframe" );
+    selectedTimeframe = target.innerHTML.toLowerCase();
+
+    setTimeframes();
+}
 
 const ellipsisImgElems = document.getElementsByClassName( "ellipsis-img" );
 
@@ -25,4 +77,10 @@ for( let i=0; i < ellipsisImgElems.length; i++ ){
     ellipsisImg.addEventListener( "mouseout", function(e){
        this.parentElement.classList.remove( "ellipsis-hovered" );
     });
+}
+
+const menuItems = document.getElementById( "menu" ).children;
+
+for( let i=0; i < menuItems.length; i++ ){
+    menuItems[i].addEventListener( "click", (e) => { e.preventDefault(); clickMenu(e); });
 }

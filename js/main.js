fbe4ea9b2087b13516529330593c3ed33499a44d
Tel <telsabate.business@gmail.com>
2025-04-06 20:30:59 +0800

    Changed data.json fetch path.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ let timeframeData;
 let selectedTimeframe = "weekly";
 
 function getData(){
-    fetch('/data.json').then((response) => {  
+    // fetch('/data.json').then((response) => {  
+    fetch('./data.json').then((response) => {  
         if(!response.ok) return console.log('Oops! Something went wrong.');
     
         return response.json();
